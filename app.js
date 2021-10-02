ddf13bf520dc5f1c37702d6360cc3b86f2972f41
motakashi <takashi_home_sky@yahoo.co.jp>
2021-10-02 15:56:52 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function fibonacci(n){
+    if(n === 0){
+        return 0;
+    }
+
+    if(n === 1){
+        return 1;
+    }
+
+    return fibonacci(n-2)+fibonacci(n-1);
+
+}
+
+const length = 40;
+for (let index = 0; index < length; index++) {
+    console.log(fibonacci(index));
+}
